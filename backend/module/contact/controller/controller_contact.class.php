aebedd0d867309a122117ca9948b046259aefb9c
Joana <joamahiques@gmail.com>
2019-05-03 09:35:09 +0200

    home y contact en angularjs, pagination en home, formulario de contact con toastr y borrado al enviarse el mensaje

--- backend/module/contact/controller/controller_contact.class.php
+++ backend/module/contact/controller/controller_contact.class.php
@@ -7,14 +7,14 @@
             $_SESSION['module'] = "contact";
         }
     
-        function list_contact() {
+        // function list_contact() {
             
-            require_once(VIEW_PATH_INC . "top-page.php");
-            require_once(VIEW_PATH_INC . "header.php");
-            require_once(VIEW_PATH_INC . "menu.php");
-            include(MODULE_VIEW_PATH . "contactus.html");
-            require_once(VIEW_PATH_INC . "footer.php");
-        }
+        //     require_once(VIEW_PATH_INC . "top-page.php");
+        //     require_once(VIEW_PATH_INC . "header.php");
+        //     require_once(VIEW_PATH_INC . "menu.php");
+        //     include(MODULE_VIEW_PATH . "contactus.html");
+        //     require_once(VIEW_PATH_INC . "footer.php");
+        // }
 
         function send_form(){
 			$name =$_POST["name"];
@@ -31,28 +31,30 @@
 			);
 			set_error_handler('ErrorHandler');
 				try{
-					enviar_email($arrArgument);
-					echo json_encode('Mensaje enviado');
+					$send=enviar_email($arrArgument);
+					echo('True');
+					exit;
 				} catch (Exception $e) {
-					echo json_encode('Server error. Try later...');
+					echo('false');
+					exit;
 				}
 			restore_error_handler();
 
-			$arrArgument = array(
-				'type' => 'admin',
-				'token' => '',
-				'inputName' => $name,
-				'inputEmail' => $mail,
-				'inputSubject' => $option,
-				'inputMessage' => $message
-			);
-			set_error_handler('ErrorHandler');
-			try{
-	            enviar_email($arrArgument);
-			} catch (Exception $e) {
-				echo json_encode('Server error. Try later...');
-			}
-			restore_error_handler();
+			// $arrArgument = array(
+			// 	'type' => 'admin',
+			// 	'token' => '',
+			// 	'inputName' => $name,
+			// 	'inputEmail' => $mail,
+			// 	'inputSubject' => $option,
+			// 	'inputMessage' => $message
+			// );
+			// set_error_handler('ErrorHandler');
+			// try{
+	        //     enviar_email($arrArgument);
+			// } catch (Exception $e) {
+			// 	echo json_encode('Server error. Try later...');
+			// }
+			// restore_error_handler();
 		}
     }
-?>  
+?>
--- frontend/modules/contact/controller/contactCtrler.js
+++ frontend/modules/contact/controller/contactCtrler.js
@@ -7,9 +7,24 @@ eden.controller('contactCtrler', function($scope, $http, ngDialog,services){
         inputMessage: ""
     };
     $scope.SubmitContact = function () {
-        var data = {"inputName": $scope.contact.inputName, "inputEmail": $scope.contact.inputEmail, 
-        "inputSubject": $scope.contact.inputSubject, "inputMessage": $scope.contact.inputMessage,"token":'contact_form'};
+        var data = {"name": $scope.contact.inputName, "email": $scope.contact.inputEmail, 
+        "opcontact": $scope.contact.inputSubject, "mess": $scope.contact.inputMessage,"token":'contact_form'};
         var contact_form = JSON.stringify(data);
         console.log(contact_form);
+        services.post('contact', 'send_form', contact_form).then(function (response) {
+                console.log(response);
+                if (response === 'True') {
+                    $scope.contact = {};
+                    $scope.form.$setPristine();///borramos formulario
+                    $scope.form.$setUntouched();////sin $error
+                    $scope.form = angular.copy($scope.contact);
+                    toastr.success('Mensaje enviado correctamente','En breve nos pondremos en contacto con usted');
+                } else {
+                    console.log('not true');
+                    toastr.success('Mensaje No Enviado','Pruebe más tarde');
+                }
+        });
+
+        
     };
 });
